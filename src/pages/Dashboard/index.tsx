398b6196ffafe7142cad1f2b0294651bfeb3b449
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2020-07-18 15:25:41 -0300

    Style dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore respositórios no Github</Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository() { }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/5720620?s=460&u=c29256a23bb9c5508caadc85cb8d0fa79fd69745&v=4"
+            alt="Gabriel Maquiavelli"
+          />
+          <div>
+            <strong>maquiavelli/test</strong>
+            <p>Just data.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
